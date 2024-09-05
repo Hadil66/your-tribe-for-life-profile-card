ec1d802249a66d9678909914960a1a3bed963fa2
Hadil24A <hadil.ahmed@hva.nl>
2024-09-05 13:25:34 +0200

    closed #2

---
+++ src/routes/+page.js
@@ -0,0 +1,11 @@
+// /** @type {import('./$types').PageLoad} */
+// import getDirectusInstance from '$lib/directus';
+// import { readItems } from '@directus/sdk';
+// export async function load({ fetch }) {
+// 	const directus = getDirectusInstance(fetch);
+// 	return {
+// 		global: await directus.request(readItems('global')),
+// 	};
+// }
+
+export let csr = false
